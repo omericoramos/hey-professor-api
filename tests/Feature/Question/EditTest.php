4693dc0edd188982851c15e16269e333988d5d12
omericoramos <omericoramos@gmail.com>
2025-02-18 18:02:08 -0300

    should be able to update a question and return a 200 status code with the updated data

--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -23,3 +23,30 @@ it('should be able to update a question', function () {
         'user_id' => $user->id,
     ]);
 });
+it('should be able to update a question and return a 200 status code with the updated data', function () {
+    $user = User::factory()->create();
+    $question = Question::factory()->create([
+        'user_id' => $user->id,
+        'question' => 'Pregunta original?'
+    ]);
+
+    Sanctum::actingAs($user, ['*']);
+
+    $response = putJson(route('question.update', $question), [
+        'question' => 'Actualizado?',
+    ])->assertOk();
+
+    $response->assertJson([
+        'data' => [
+            'id' => $question->id,
+            'question' => 'Actualizado?',
+            'status' => $question->status,
+            'created_by' => [
+                'id' => $user->id,
+                'name' => $user->name
+            ],
+            'created_at' => $question->created_at->format('Y-m-d H:i'),
+            'updated_at' => $question->updated_at->format('Y-m-d H:i')
+        ]
+    ]);
+});
